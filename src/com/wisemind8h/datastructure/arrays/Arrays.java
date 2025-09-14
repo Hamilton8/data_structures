ddcc3de47bb8fc9b993ccb19aaa46a24a1bdccaf
Hamilton Guirrugo <guirrugohamiltonantonio@gmail.com>
2025-09-14 08:26:15 -0400

    Better version of method add

--- src/com/wisemind8h/datastructure/arrays/Arrays.java
+++ src/com/wisemind8h/datastructure/arrays/Arrays.java
@@ -2,18 +2,33 @@ package com.wisemind8h.datastructure.arrays;
 
 public class Arrays {
     private String[] elements;
+    private int size;
 
     public Arrays(int capacity){
         this.elements = new String[capacity];
+        this.size = 0;
     }
 
-    public void add(String element){
+    public void add(String element)throws Exception{
+        if (size<this.elements.length){
+            this.elements[this.size] = element;
+            size++;
+            return;
+        }
+        throw new ArrayStoreException("The array is full!");
+    }
+
+
+    public String[] getElements(){
+        return this.elements;
+    }
+
+    /*public void add(String element){
         for (int i = 0; i < this.elements.length; i++) {
             if (this.elements[i]==null){
                 this.elements[i]=element;
                 return;
             }
         }
-    }
-
+    }*/
 }
--- src/com/wisemind8h/datastructure/arrays/test/Test01.java
+++ src/com/wisemind8h/datastructure/arrays/test/Test01.java
@@ -1,4 +1,21 @@
 package com.wisemind8h.datastructure.arrays.test;
 
+import com.wisemind8h.datastructure.arrays.Arrays;
+
 public class Test01 {
+    public static void main(String[] args) {
+        Arrays arrays = new Arrays(2);
+        try {
+            arrays.add("Element 01");
+            arrays.add("Element 02");
+            arrays.add("Element 03");
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+
+
+        for (String elem: arrays.getElements()){
+            System.out.println(elem);
+        }
+    }
 }
